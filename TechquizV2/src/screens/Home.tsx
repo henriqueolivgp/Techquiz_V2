35dc480acd88f99fa85b69003070a79361b49a44
h3nrique <henrique.o@ipvc.pt>
2023-07-04 18:00:05 +0100

    responsive home

--- TechquizV2/src/screens/Home.tsx
+++ TechquizV2/src/screens/Home.tsx
@@ -8,8 +8,8 @@ function Home() {
     <>
       <Navbar />
       <section className="banner  bg-violet-100 h-96">
-        <div className=" flex flex-col xl:flex-row" >
-          <div className=" w-[853px] h-96 p-6 xl:w-auto xl:h-96 ">
+        <div className=" flex flex-col xl:flex-row sm:relative" >
+          <div className=" xl-w-[853px] lg-w-[853px] h-96 p-6 xl:w-auto xl:h-96 ">
             <img className="h-20" src="/src/assets/TechquizBlack.png" alt="" />
             <p className="mt-5">Com o Techquiz pode avaliar o  seu conhecimento e saber o seu nível em todas as diversas áreas de informática.</p>
             <button type="button" className="h-12 mt-5 focus:outline-none text-white bg-purple-700 hover:bg-purple-800 focus:ring-4 focus:ring-purple-300 font-medium rounded-lg text-sm px-5 py-2.5 mb-2 dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-900">Começar agora!</button>
@@ -19,7 +19,7 @@ function Home() {
           </div>
         </div>
       </section>
-      <div className="container mx-auto">
+      <div className="container mx-auto ">
         <div className="content">
 
         </div>
