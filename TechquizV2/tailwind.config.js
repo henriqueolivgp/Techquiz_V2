38c17147130ab7ed37f3a4c54d633d33c7e69270
henriqueolivgp <henrique.o@ipvc.pt>
2023-07-06 18:43:44 +0100

    add react-router-dom

--- TechquizV2/tailwind.config.js
+++ TechquizV2/tailwind.config.js
@@ -9,9 +9,25 @@ export default {
   theme: {
     extend: {},
     // Define os breakpoints personalizados
-    breakpoints: {
-      xs: "475px", // ou qualquer valor que você preferir
-    },
+    screens: {
+      'xs': '475px',
+      // => @media (min-width: 470px) { ... }
+
+      'sm': '640px',
+      // => @media (min-width: 640px) { ... }
+
+      'md': '768px',
+      // => @media (min-width: 768px) { ... }
+
+      'lg': '1024px',
+      // => @media (min-width: 1024px) { ... }
+
+      'xl': '1280px',
+      // => @media (min-width: 1280px) { ... }
+
+      '2xl': '1536px',
+      // => @media (min-width: 1536px) { ... }
+    }
   },
   plugins: [
     flowbitePlugin
